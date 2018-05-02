8f5b1802582cc216945db4c343dcbdb768898b72
alu0100812428 <alu0100812428@ull.edu.es>
2018-05-02 08:30:41 +0000

    Modificando el server y routes, añadiendo fichero de passport

---
+++ app/routes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+
+var path = require('path');
+var multer = require('multer');
+
+
+// ---------- LOGIN ----------
+router.get('/login', function(req, res, next) {
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/inicio',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+
+module.exports = router;
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+      return next();
+  res.redirect('/login');
+}
--- server.js
+++ server.js
@@ -1,8 +1,45 @@
-var express = require('express')
-var app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Servidor express funcionando')
-})
- 
-app.listen(8080)
+var express = require('express');
+var path = require('path');
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var session = require('express-session');
+
+var routes = require('./app/routes');
+
+var configDB = require('./config/database.js');
+mongoose.connect(configDB.url);
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({ secret: 'shhsecret', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
+
+app.use('/', routes);
+
+app.listen(port);
+
+
+module.exports = app;
