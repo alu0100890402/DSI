d75f9dd700623e1b8c1eaf8e2a2c75a69c9282ec
alu0100812428 <alu0100812428@ull.edu.es>
2018-05-27 10:23:11 +0000

    Añadida la expresion regular de la password

--- config/passport.js
+++ config/passport.js
@@ -40,6 +40,13 @@ module.exports = function(passport) {
         if (user) {
           return done(null, false, req.flash('signupMessage', 'Este correo está asociado a otro usuario.'));
         } else {
+          if(/^([a-zA-Z0-9]{6,})$/.test(req.body.password)){
+            console.log("TRUE")
+          }
+          else{
+            return done(null, false, req.flash('signupMessage', 'Esta contraseña no es válida'));
+            console.log("False");
+          }
           var newUser = new User();
           newUser.local.name = req.body.name;
           newUser.local.surname = req.body.surname;
